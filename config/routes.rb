0f21d941424ebd38345a67deccf1c0bdd4fa489a
bkn.ngeno@gmail.com <bkn.ngeno@gmail.com>
2022-11-29 10:08:38 +0300

    wip: updated the routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,3 +8,14 @@ Rails.application.routes.draw do
   # get "/user/:id", to: "rails#show"
   resources :users, only:[:index, :show, :create, :update]
 end
+
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # custom route
+  # get "/" , to: "rails#index"
+  # get "/user/:id", to: "rails#show"
+  resources :users, only:[:index, :show, :create, :update]
+end
